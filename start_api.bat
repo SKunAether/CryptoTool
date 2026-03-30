8f2c5cd3a0a49dba5e3e76c19f7380e515036f09
SKunAether <2820241167@qq.com>
2026-03-30 15:44:55 +0800

    修复批处理文件

--- start_api.bat
+++ start_api.bat
@@ -8,11 +8,15 @@ echo.
 
 cd /d "%~dp0"
 
-echo [1/2] 激活虚拟环境...
-call venv\Scripts\activate.bat
+echo [1/2] 检查虚拟环境...
+if not exist "venv\Scripts\python.exe" (
+    echo 错误: 虚拟环境不存在，请先创建虚拟环境
+    pause
+    exit /b 1
+)
 
 echo [2/2] 启动API服务器...
 echo.
-python plugins\burp_extension.py
+venv\Scripts\python.exe plugins\burp_extension.py
 
 pause
